a4959167aea843a8c1dc39bd6eff078c8e4778fa
18395595415@163.com <18395595415@163.com>
2023-08-07 09:55:44 +0800

    idea测试一下

--- Yuuu.java
+++ Yuuu.java
@@ -4,6 +4,7 @@
  */
 public class Yuuu {
     public static void main(String[] args) {
+        System.out.println("===============");
         System.out.println("YuDaWang");
         System.out.println("YuDaWang");
     }

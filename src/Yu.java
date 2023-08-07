4d14b9771e70215e4b5b026fbe9ab249282c0448
18395595415@163.com <18395595415@163.com>
2023-08-07 10:25:28 +0800

    YYYYY分支提交测试

---
+++ src/Yu.java
@@ -0,0 +1,9 @@
+/**
+ * @author yuzeya
+ * @date 2023/8/7 10:21
+ */
+public class Yu {
+    public static void main(String[] args) {
+        System.out.println("=====HAHAHAHAHA==============");
+    }
+}
